6f02cbb56c54f3e613396e8efb96afb6c2575e0a
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-01 02:41:53 +0100

    seperate welcome page components to separate blade files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,43 +1,8 @@
 @extends('layouts.home')
 
 @section('content')
-  <!-- Countdown container -->
-  <div class="countdown-container">
-    <div class="container">
-      <div class="row">
-        <div class="col-6 col-sm-6 col-lg-3">
-          <div class="count-down">
-            <h4 class="heading-text" id="countDay"></h4>
-            <span class="desc-text">DAYS</span>
-          </div>
-        </div>
 
-        <div class="col-6 col-sm-6 col-lg-3">
-          <div class="count-down">
-            <h4 class="heading-text" id="countHrs"></h4>
-            <span class="desc-text">HOURS</span>
-          </div>
-        </div>
-
-        <div class="col-6 col-sm-6 col-lg-3">
-          <div class="count-down">
-            <h4 class="heading-text" id="countMin"></h4>
-            <span class="desc-text">MINUTES</span>
-          </div>
-        </div>
-
-        <div class="col-6 col-sm-6 col-lg-3">
-          <div class="count-down">
-            <h4 class="heading-text" id="countSec"></h4>
-            <span class="desc-text">SECONDS</span>
-          </div>
-        </div>
-
-
-      </div>
-    </div>
-  </div>
-  <!-- /Countdown container -->
+  @include('compo.countdown')
 
   <!-- Coming soon content -->
   <div class="coming-soon-content">
@@ -80,41 +45,7 @@
   </div>
   <!-- /Coming soon content -->
 
-  <!-- Social icons -->
-  <div class="container">
-    <div class="row">
-      <div class="social-icons">
-        <div class="social-icon">
-          <a href="https://facebook.com/Hostel-Paddy-111950534317618/?_rdc=1&_rdr">
-            <img src="{{ asset('files/assets/Facebook.svg') }}" alt="Facebook">
-          </a>
-        </div>
+  @include('compo.social')
 
-        <div class="social-icon">
-          <a href="https://twitter.com/hostelpaddy">
-            <img src="{{ asset('files/assets/Twitter.svg') }}" alt="Twitter">
-          </a>
-        </div>
-
-        <div class="social-icon">
-          <a href="https://instagram.com/hostelpaddy">
-            <img src="{{ asset('files/assets/Instagram.svg') }}" alt="Instagram">
-          </a>
-        </div>
-
-      </div>
-    </div>
-  </div>
-  <!-- /Social icons -->
-
-
-  <!-- Copyright area -->
-  <div class="container">
-    <div class="row text-center">
-      <div class="copyright-area">
-        &copy; CHERNOBYL, <span id="copyright"></span>
-      </div>
-    </div>
-  </div>
-  <!-- /Copyright area -->
+  @include('compo.copyright')
 @endsection
