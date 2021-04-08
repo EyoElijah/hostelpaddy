206cb6ef7952d736941ddf55111fc66b58655219
Ekene <ekennyobiasogu@gmail.com>
2021-04-08 07:19:05 +0100

    student auth fixed

--- database/migrations/2021_04_07_190647_create_agents_table.php
+++ database/migrations/2021_04_07_190647_create_agents_table.php
@@ -16,7 +16,7 @@ class CreateAgentsTable extends Migration
         Schema::create('agents', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->nullable();
+            $table->string('email')->unique();
             $table->string('phone');
             $table->string('password');
             $table->string('image');
