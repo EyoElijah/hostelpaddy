fa597db0a236dcea7e5606358b4b8b4f4ed5fed4
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-09 10:53:29 +0100

    move comingsoon components

--- resources/views/components/comingsoon/social.blade.php
+++ resources/views/components/comingsoon/social.blade.php
@@ -4,19 +4,19 @@
     <div class="social-icons">
       <div class="social-icon">
         <a href="https://facebook.com/Hostel-Paddy-111950534317618/?_rdc=1&_rdr">
-          <img src="{{ asset('files/assets/Facebook.svg') }}" alt="Facebook">
+          <img src="{{ asset('comingsoon/assets/Facebook.svg') }}" alt="Facebook">
         </a>
       </div>
 
       <div class="social-icon">
         <a href="https://twitter.com/hostelpaddy">
-          <img src="{{ asset('files/assets/Twitter.svg') }}" alt="Twitter">
+          <img src="{{ asset('comingsoon/assets/Twitter.svg') }}" alt="Twitter">
         </a>
       </div>
 
       <div class="social-icon">
         <a href="https://instagram.com/hostelpaddy">
-          <img src="{{ asset('files/assets/Instagram.svg') }}" alt="Instagram">
+          <img src="{{ asset('comingsoon/assets/Instagram.svg') }}" alt="Instagram">
         </a>
       </div>
 
