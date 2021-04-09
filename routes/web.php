7672ad5655aeaa6c13242170a05ce6be552ec927
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-09 14:14:11 +0100

    add namedroute to landing page home

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('home');
 
 Route::get('/coming-soon', function () {
     return view('coming-soon');
